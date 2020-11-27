41809daf52da25cd0208b1e8bbf9edf11b94eb87
Sahil Jain <jainsahil77@gmail.com>
2020-11-27 12:49:18 +0530

    Add files via upload

---
+++ src/main/java/com/sample/oauth/OAuthSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sample.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthSampleApplication.class, args);
+	}
+
+}
